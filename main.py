2cdff43798ae0224ab5bc7c3061903a41f21d3e1
Rafa Bacano <rafaelsepanasantunes@gmail.com>
2023-04-24 20:46:51 +0100

    organization and optimization of code

--- main.py
+++ main.py
@@ -5,104 +5,144 @@ import tkinter.ttk as ttk
 import datetime
 from PIL import Image, ImageTk
 
-#Vars
 root = tk.Tk()
 home_dir = path.expanduser('~')
 desktop_path = os.path.join(home_dir, 'Desktop')
-    #Images
+
+#Images
 arrow = ImageTk.PhotoImage(Image.open('images/arrow_white_theme.png'))
 arrow_on = ImageTk.PhotoImage(Image.open('images/arrow_on_white_theme.png'))
 fliparrow = ImageTk.PhotoImage(Image.open('images/arrow_white_theme.png').rotate(180))
 fliparrow_on = ImageTk.PhotoImage(Image.open('images/arrow_on_white_theme.png').rotate(180))
 uparrow = ImageTk.PhotoImage(Image.open('images/arrow_white_theme.png').rotate(-90))
+darktheme = ImageTk.PhotoImage(Image.open('images/dark_theme_icon.png'))
+whitetheme = ImageTk.PhotoImage(Image.open('images/dark_theme_icon.png'))
 
-    #Window Settings
+#Window Settings
 root.title('File Explorer')
 root.geometry('960x540')
 root.minsize(640,360)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+root.columnconfigure(3, weight=1)
+root.columnconfigure(4, weight=1)
+root.rowconfigure(1, weight=1)
+
+#Top setup
+top_panel = tk.PanedWindow(root)
+top_panel.grid(row=0, column=1, sticky='nsew', pady=10, columnspan=4)
+
+    #Left Widgets
+backarrow = tk.Button(root, image=arrow)
+backarrow.grid(row=0, column=0, sticky='nsw', padx=(5,5), pady=10)
+frontarrow = tk.Button(root, image=fliparrow)
+frontarrow.grid(row=0, column=0, padx=(25,0), sticky='nsw', pady=10)
+toparrow = tk.Button(root, image=uparrow)
+toparrow.grid(row=0, column=0, padx=(50,0), sticky='nsw', pady=10)
+themebutton = tk.Button(root, image=darktheme)
+themebutton.grid(row=0, column=0, padx=(75,5), sticky='nsw', pady=10)
+
+        #Left Widgets Settings
+
+backarrow.configure(relief='flat')
+frontarrow.configure(relief='flat')
+toparrow.configure(relief='flat')
+themebutton.configure(relief='flat')
+
+
+    #Path Text
+path_text = tk.Frame(top_panel)
+
+        #Path Text Widget
+path_text_widget = tk.Entry(path_text, width=100)
+path_text_widget.pack(side='left', fill='both', expand=True, padx=5)
+
+    #Search Text
+search_text = tk.Frame(top_panel)
+
+        #Search Text Widget
+search_text_widget = tk.Entry(search_text)
+search_text_widget.pack(side='right', fill='both',expand=True, padx=5)
+
+        #Top Panel Settings
+top_panel.add(path_text)
+top_panel.add(search_text)
+top_panel.paneconfigure(path_text, minsize=200)
+top_panel.paneconfigure(search_text, minsize=200)
+
+
+
+
+
 
 #Bottom setup
-paned = tk.PanedWindow(root)
-paned.grid(row=1, column=0, columnspan=5, sticky='nsew')
+bottom_panel = tk.PanedWindow(root)
+bottom_panel.grid(row=1, column=0, columnspan=5, sticky='nsew')
 
-frame1 = tk.Frame(paned)
-treeview = ttk.Treeview(frame1)
-treeview.heading('#0', text='Quick Acess', anchor='w')
-treeview.pack(side='left', fill='both',expand=True)
+    #Left panel
+quick_acess = tk.Frame(bottom_panel)
 
-scrollbar1 = ttk.Scrollbar(frame1, orient='vertical', command=treeview.yview)
-scrollbar1.pack(side='right', fill='y')
+        #quick acess widget
+quick_acess_widget = ttk.Treeview(quick_acess)
+quick_acess_widget.heading('#0', text='Quick Acess', anchor='c')
+quick_acess_widget.pack(side='left', fill='both',expand=True)
 
-treeview.configure(yscrollcommand=scrollbar1.set)
 
-node = treeview.insert('', 'end', text='This PC', open=True)
+        #quick acess scrollbar
+quick_acess_scrollbar = ttk.Scrollbar(quick_acess_widget, orient='vertical', command=quick_acess_widget.yview)
+quick_acess_scrollbar.pack(side='right', fill='y')
+quick_acess_widget.configure(yscrollcommand=quick_acess_scrollbar.set)
 
-for i in os.listdir(home_dir):
-    match i:
-        case "Desktop":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
-        case "Documents":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
-        case "Pictures":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
-        case "Music":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
-        case "3D Objects":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
-        case "Downloads":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
-        case "Videos":
-            node2 = treeview.insert(node, 'end', text=i, open=False)
-            for entry in os.listdir(home_dir + "/" + i):
-                if path.isdir(path.join(home_dir, i, entry)):
-                    treeview.insert(node2, 'end', text=entry, open=False)
+            #quick acess folder insertion
+this_pc = quick_acess_widget.insert('', 'end', text='This PC', open=True)
+directories = ['Desktop', 'Documents', 'Pictures', 'Music', '3D Objects', 'Downloads', 'Videos']
 
+                #Default system folder insertion
+for dir in directories:
+    if path.exists(path.join(home_dir, dir)):
+        subfolder = quick_acess_widget.insert(this_pc, 'end', text=dir, open=False)
+        for entry in os.listdir(path.join(home_dir, dir)):
+            if path.isdir(path.join(home_dir, dir, entry)):
+                quick_acess_widget.insert(subfolder, 'end', text=entry, open=False)
+
+                #Disk insertion
 for i in range(65, 91):
     drive_name = chr(i) + ':'
     if os.path.exists(drive_name):
-        node2 = treeview.insert(node, 'end', text=f"Disk ({drive_name})", open=False)
+        subfolder = quick_acess_widget.insert(this_pc, 'end', text=f"Disk ({drive_name})", open=False)
         for entry in os.scandir(drive_name):
             if entry.is_dir() and not entry.stat().st_file_attributes & 2:
-                treeview.insert(node2, 'end', text=entry.name, open=False)
+                quick_acess_widget.insert(subfolder, 'end', text=entry.name, open=False)
 
-frame2 = tk.Frame(paned, relief="flat")
-tree = ttk.Treeview(frame2)
-tree.pack(side='left', fill='both',expand=True)
 
-scrollbar = ttk.Scrollbar(tree, orient='vertical', command=tree.yview)
-scrollbar.pack(side='right', fill='y')
 
-tree.configure(yscrollcommand=scrollbar.set)
+    #Right Panel
+folder_view = tk.Frame(bottom_panel, relief="flat")
 
-tree['columns'] = ('1', '2', '3', '4')
-tree['show'] = 'headings'
-tree.column('1', anchor='w', stretch=0, minwidth=50)
-tree.column('2', anchor='w', stretch=0, minwidth=50)
-tree.column('3', anchor='w', stretch=0, minwidth=50, width=120)
-tree.column('4', anchor='e', stretch=0, minwidth=50)
-tree.heading('1', text='Name', anchor='w')
-tree.heading('2', text='Date of Modification', anchor='w')
-tree.heading('3', text='Type', anchor='w')
-tree.heading('4', text='Size', anchor='w')
+        #folder view widget
+folder_view_widget = ttk.Treeview(folder_view)
+folder_view_widget.pack(side='left', fill='both',expand=True)
 
+        #folder view scrollbar
+folder_view_scrollbar = ttk.Scrollbar(folder_view_widget, orient='vertical', command=folder_view_widget.yview)
+folder_view_scrollbar.pack(side='right', fill='y')
+folder_view_widget.configure(yscrollcommand=folder_view_scrollbar.set)
+
+            #folder view folders setup
+
+                #columns
+folder_view_widget['columns'] = ('1', '2', '3', '4')
+folder_view_widget['show'] = 'headings'
+folder_view_widget.column('1', anchor='w', stretch=0, minwidth=50)
+folder_view_widget.column('2', anchor='w', stretch=0, minwidth=50)
+folder_view_widget.column('3', anchor='w', stretch=0, minwidth=50, width=120)
+folder_view_widget.column('4', anchor='e', stretch=0, minwidth=50)
+folder_view_widget.heading('1', text='Name', anchor='w')
+folder_view_widget.heading('2', text='Date of Modification', anchor='w')
+folder_view_widget.heading('3', text='Type', anchor='w')
+folder_view_widget.heading('4', text='Size', anchor='w')
+                
+                #folders
 for entry in os.scandir(desktop_path):
     time = datetime.datetime.fromtimestamp(path.getmtime(os.path.join(desktop_path, entry.name))).strftime('%d/%m/%Y %H:%M')
     size = path.getsize(entry)
@@ -116,52 +156,15 @@ for entry in os.scandir(desktop_path):
     
     if path.isdir(os.path.join(desktop_path, entry.name)):
         type = 'File Folder'
-        tree.insert('','end',values=(entry.name, time, type, size))
+        folder_view_widget.insert('','end',values=(entry.name, time, type, size))
     else:
         name, type = entry.name.rsplit('.', 1)
-        tree.insert('','end',values=(entry.name, time, '.' + type + ' File', size))
+        folder_view_widget.insert('','end',values=(entry.name, time, '.' + type + ' File', size))
 
-paned.add(frame1)
-paned.add(frame2)
-
-paned.paneconfigure(frame1, minsize=50)
-paned.paneconfigure(frame2, minsize=50)
-
-paned1 = tk.PanedWindow(root)
-paned1.grid(row=0, column=1, sticky='nsew', pady=10, columnspan=4)
-
-frame3 = tk.Frame(paned1)
-frame4 = tk.Frame(paned1)
-
-backarrow = tk.Button(root, image=arrow)
-backarrow.grid(row=0, column=0, sticky='nsw', padx=(10,5), pady=10)
-
-frontarrow = tk.Button(root, image=fliparrow)
-frontarrow.grid(row=0, column=0, padx=(30,0), sticky='nsw', pady=10)
-
-toparrow = tk.Button(root, image=uparrow)
-toparrow.grid(row=0, column=0, padx=(55,5), sticky='nsw', pady=10)
-
-input1 = tk.Entry(frame3, width=100)
-input1.pack(side='left', fill='both', expand=True, padx=5)
-
-input2 = tk.Entry(frame4)
-input2.pack(side='right', fill='both',expand=True, padx=5)
-
-backarrow.configure(relief='flat')
-frontarrow.configure(relief='flat')
-toparrow.configure(relief='flat')
-
-paned1.add(frame3)
-paned1.add(frame4)
-
-paned1.paneconfigure(frame3, minsize=200)
-paned1.paneconfigure(frame4, minsize=200)
-
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=1)
-root.columnconfigure(3, weight=1)
-root.columnconfigure(4, weight=1)
-root.rowconfigure(1, weight=1)
+    #Bottom Panel Settings Setup
+bottom_panel.add(quick_acess, width=200)
+bottom_panel.add(folder_view)
+bottom_panel.paneconfigure(quick_acess, minsize=50)
+bottom_panel.paneconfigure(folder_view, minsize=50)
 
 root.mainloop()
